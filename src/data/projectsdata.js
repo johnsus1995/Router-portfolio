253bc4244a5c613e32d851912ec22fa60d0943f1
jaison john <thejohnjaison@yahoo.com>
2021-09-19 16:19:55 +0530

    all images changed

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -15,7 +15,7 @@ export const projectsdata =[
         desc:"In this project I have made use of most commonly use hooks and custome hook. This UI is very attractive and validation is done with REGEX",
         github:"https://github.com",
         demo:"https://form-validation-advanced.netlify.app/",
-        image:"./images/project-2.jpeg"
+        image:"https://images.unsplash.com/photo-1624455806586-037944b1fa1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"
     },
     {
         id:uuidv4(),
@@ -23,7 +23,7 @@ export const projectsdata =[
         desc:"This is static page made as part of my initial learning and its responsive",
         github:"https://github.com",
         demo:"https://johnsus1995.github.io/Tribute-page/",
-        image:"./images/project-3.jpeg"
+        image:"https://images.pexels.com/photos/3098683/pexels-photo-3098683.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     },
     {
         id:uuidv4(),
@@ -31,7 +31,7 @@ export const projectsdata =[
         desc:"In this project I have made use of local storage to store users data who login to the website",
         github:"https://github.com",
         demo:"https://form-validation-basic.web.app/",
-        image:"./images/project-4.jpeg"
+        image:"https://images.pexels.com/photos/5946167/pexels-photo-5946167.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
     },
     {
         id:uuidv4(),
@@ -47,6 +47,6 @@ export const projectsdata =[
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
         github:"https://github.com",
         demo:"https://amazon.in",
-        image:"./images/project-6.jpeg"
+        image:"https://images.pexels.com/photos/4019400/pexels-photo-4019400.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     },
 ]
