93357c21cb71afbb62873d2d6f92960f0edfb3f6
jaison john <thejohnjaison@yahoo.com>
2021-09-19 15:10:05 +0530

    changed project names and hosted addresses

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -3,47 +3,47 @@ import {v4 as uuidv4} from 'uuid'
 export const projectsdata =[
     {
         id:uuidv4(),
-        title:"Project 1",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+        title:"Search filter",
+        desc:"Implemented the search filters used in eCommerse applications. You can search anything and matching results will be sorted",
         github:"https://github.com",
-        demo:"https://amazon.in",
+        demo:"https://react-search-filter.netlify.app/",
         image:"./images/project-1.jpeg"
     },
     {
         id:uuidv4(),
-        title:"Project 2",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+        title:"Advanced form validation",
+        desc:"In this project I have made use of most commonly use hooks and custome hook. This UI is very attractive and validation is done with REGEX",
         github:"https://github.com",
-        demo:"https://amazon.in",
+        demo:"https://form-validation-advanced.netlify.app/",
         image:"./images/project-2.jpeg"
     },
     {
         id:uuidv4(),
-        title:"Project 3",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+        title:"Static Tribute page",
+        desc:"This is static page made as part of my initial learning and its responsive",
         github:"https://github.com",
-        demo:"https://amazon.in",
+        demo:"https://johnsus1995.github.io/Tribute-page/",
         image:"./images/project-3.jpeg"
     },
     {
         id:uuidv4(),
-        title:"Project 4",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+        title:"Form validation basic",
+        desc:"In this project I have made use of local storage to store users data who login to the website",
         github:"https://github.com",
-        demo:"https://amazon.in",
+        demo:"https://form-validation-basic.web.app/",
         image:"./images/project-4.jpeg"
     },
     {
         id:uuidv4(),
-        title:"Project 5",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+        title:"Amazon UI clone",
+        desc:"This is a replica of amazon and UI is almost similar, I have made use of comples hooks in this project",
         github:"https://github.com",
-        demo:"https://amazon.in",
+        demo:"https://my-d2f06.web.app/",
         image:"./images/project-5.jpeg"
     },
     {
         id:uuidv4(),
-        title:"Project 6",
+        title:"In progress",
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
         github:"https://github.com",
         demo:"https://amazon.in",
